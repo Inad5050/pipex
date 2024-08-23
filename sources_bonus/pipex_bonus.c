17d9d17669579a92c3c4fcb746e40873e5d2000b
Inad <danitorralba90@gmail.com>
2024-08-23 17:33:02 +0200

    entregable

--- sources_bonus/pipex_bonus.c
+++ sources_bonus/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dani <dani@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 04:22:22 by dani              #+#    #+#             */
-/*   Updated: 2024/08/23 17:29:58 by dani             ###   ########.fr       */
+/*   Updated: 2024/08/23 17:31:58 by dani             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,36 +22,19 @@ void	pipex(t_pipex *p)
 
 	i = 0;
 	while (i < p->cmd_n)
-	{
 		child_select(i++, p);
-		perror("pre WAIT");
-		waitpid(-1, NULL, 0);
-	}
-	/* waitpid(-1, NULL, 0); */
-/* 	perror("pre WAIT");
-	wait(NULL); */
+	waitpid(-1, NULL, 0);
 }
-
-/* void	pipex(t_pipex *p)
-{
-	int	i;
-
-	i = 0;
-	while (i < p->cmd_n)
-		child_select(i++, p);
-	perror("pre WAIT");
-	wait(NULL);
-} */
 
 void	child_select(int i, t_pipex *p)
 {
-	/* pid_t	pid; */
+	pid_t	pid;
 
-	p->pid = fork();
+	pid = fork();
 	ft_printf("post fork\n");
-	if (p->pid < 0)
+	if (pid < 0)
 		pipex_exit("Fork", p);
-	if (!p->pid)
+	if (!pid)
 	{
 		if (!i)
 			child_dup(p->fd_in, p->pi[i].pipefd[1], p);
@@ -63,7 +46,6 @@ void	child_select(int i, t_pipex *p)
 		if (execve(p->m[i].cmd_path, p->m[i].cmd_argv, p->envp) < 0)
 			pipex_exit("Execve", p);
 	}
-	perror("termina child_select");
 }
 
 void	child_dup(int new_in, int new_out, t_pipex *p)
@@ -73,61 +55,3 @@ void	child_dup(int new_in, int new_out, t_pipex *p)
 	if (dup2(new_out, STDOUT_FILENO) < 0)
 		pipex_exit("Dup2 child STDOUT", p);
 }
-
-/* void	child(t_pipex *p, int i)
-{
-	if (!i)
-	{
-		if (dup2(p->pi[i].pipefd[1], STDOUT_FILENO) < 0)
-			pipex_exit("Dup2 child STDOUT", p);
-		if (dup2(p->fd_in, STDIN_FILENO) < 0)
-			pipex_exit("Dup2 child STDIN", p);
-	}
-	else
-	{
-		if (dup2(p->pi[i - 1].pipefd[0], STDIN_FILENO) < 0)
-			pipex_exit("Dup2 child STDIN", p);
-		if (dup2(p->pi[i].pipefd[1], STDOUT_FILENO) < 0)
-			pipex_exit("Dup2 child STDOUT", p);
-	}
-	close_pipes(p);
-	if (execve(p->m[i].cmd_path, p->m[i].cmd_argv, p->envp) < 0)
-		pipex_exit("Execve child", p);
-	exit (EXIT_SUCCESS);
-} */
-
-/* void	last_child(t_pipex *p, int i)
-{
-	perror("last_child 1");
-	
-	if (dup2(p->pi[i - 1].pipefd[0], STDIN_FILENO) < 0)
-		pipex_exit("Dup2 last_child STDIN", p);
-	if (dup2(p->fd_out, STDOUT_FILENO) < 0)
-		pipex_exit("Dup2 last_child STDOUT", p);
-	close_pipes(p);
-	if (execve(p->m[i].cmd_path, p->m[i].cmd_argv, p->envp) < 0)
-		pipex_exit("Execve last_child", p);
-
-	perror("last_child 2");
-	
-	exit (EXIT_SUCCESS);
-} */
-
-/* void	pipex(t_pipex *p)
-{
-	p->pid = fork();
-	if (!p->pid)
-	{
-		if (p->pid < 0)
-			pipex_exit("Fork", p);
-		else if (p->pid == 0 && p->i == p->cmd_n - 1)
-			last_child(p, p->i);
-		else if (p->pid == 0)
-			child(p, p->i);
-		else
-		{
-			waitpid(pid, NULL, 0);
-		}
-		p->i++;
-	}
-} */
